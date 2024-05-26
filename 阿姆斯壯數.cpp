ddd052b5d927b62659d14c36df7c5d1d3ea1f434
harry31415926 <159117016+harry31415926@users.noreply.github.com>
2024-05-26 20:45:08 +0800

    Add files via upload

---
+++ 阿姆斯壯數.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+int compute(int n)
+{
+    int sum = 0, sum1 = 0;
+    for (int i = 1; i < n; i++)
+    {
+        string str = to_string(i);
+        for (int j = 0; j < str.size(); j++)
+            sum += pow(str[j] - '0', str.size());
+        if (sum == i)
+        {
+            cout << sum << endl;
+            sum1 += sum;
+        }
+        sum = 0;
+    }
+    return sum1;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << compute(n) << endl;
+    return 0;
+}
